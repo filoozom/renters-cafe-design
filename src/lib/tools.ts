1e49f6cff79b19bdeb30af60aa643295c6454d06
Philippe Schommers <philippe@schommers.be>
2021-12-01 13:15:27 +0100

    Sort steal auctions

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,4 +1,14 @@
+import type { BigNumber } from "@ethersproject/bignumber";
+
 export const formatNumber = (number: number) =>
   (Math.round(number * 100) / 100)
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+export const compareBigNumber = (a: BigNumber, b: BigNumber) => {
+  if (a.eq(b)) {
+    return 0;
+  }
+
+  return a.gt(b) ? 1 : -1;
+};
--- src/pages/stealing.tsx
+++ src/pages/stealing.tsx
@@ -5,18 +5,20 @@ import { useQuery } from "urql";
 import config from "../../config/default";
 import { getProvider } from "../lib/ethereum";
 import { StealAuction } from "../lib/contracts/steal-auction";
-import {
+import type {
   AuctionContract,
+  PropertyAuction,
   StealableProperty,
 } from "../components/auctions/types";
 import { Auctions } from "../components/auctions/auctions";
 import { One } from "@ethersproject/constants";
+import { compareBigNumber } from "../lib/tools";
 
 const StealingQuery = `
   query ($stealableProperties: ID!) {
     stealablePropertiesFactory(id: $stealableProperties) {
       id
-      properties(where: {owners_not: []}) {
+      properties(where: { owners_not: [] }) {
         id
         cap
         minted
@@ -98,25 +100,30 @@ const PropertyCards = () => {
       return null;
     }
 
+    const getSortKey = (auction: PropertyAuction) =>
+      auction.content[0].property.owners[0].protectedUntil;
+
     const { stealablePropertiesFactory: sp } = result.data;
-    return sp.properties.map((property) => {
-      const [owner] = property.owners;
-      return {
-        id: property.id,
-        startPrice: owner.price.mul(property.startRatio).div(10000),
-        endPrice: owner.price.mul(property.startRatio).div(10000),
-        startTimestamp: owner.protectedUntil,
-        duration: property.duration,
-        content: [
-          {
-            id: "0",
-            count: One,
-            weight: One,
-            property,
-          },
-        ],
-      };
-    });
+    return sp.properties
+      .map((property) => {
+        const [owner] = property.owners;
+        return {
+          id: property.id,
+          startPrice: owner.price.mul(property.startRatio).div(10000),
+          endPrice: owner.price.mul(property.startRatio).div(10000),
+          startTimestamp: owner.protectedUntil,
+          duration: property.duration,
+          content: [
+            {
+              id: "0",
+              count: One,
+              weight: One,
+              property,
+            },
+          ],
+        };
+      })
+      .sort((a, b) => compareBigNumber(getSortKey(a), getSortKey(b)));
   }, [result.data]);
 
   if (!auctions || !contract) {
