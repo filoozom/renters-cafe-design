9b8caa63b941b41ea0309918e16449ff910184fe
Philippe Schommers <philippe@schommers.be>
2021-12-22 15:47:30 +0100

    Remove duplicate class

--- src/components/icons/gift.tsx
+++ src/components/icons/gift.tsx
@@ -1,7 +1,6 @@
 export const GiftIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
     fill="none"
     viewBox="0 0 24 24"
     class="inline-block w-6 h-6 stroke-current"
